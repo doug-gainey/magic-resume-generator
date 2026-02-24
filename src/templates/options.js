fcaa100210d8f343595f92e3a1ae5332e76a1274
Doug Gainey <douggainey@gmail.com>
2026-02-23 21:05:55 -0500

    Remove skill sorting.

--- src/templates/options.js
+++ src/templates/options.js
@@ -148,7 +148,7 @@ export default function (name) {
           const key = obj.type;
           const curGroup = acc[key] || [];
 
-          return {...acc, [key]: vm.sort([...curGroup, obj.name])};
+          return {...acc, [key]: [...curGroup, obj.name]};
         }, {});
       }
     },
